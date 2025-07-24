5dd4eeeedc399f7c0013dc82f3732a5f59fb4651
ale.gd <ale.gueden@gmail.com>
2025-07-24 21:51:54 +0200

    refactor: move blogs component to component

--- next/ui/blog/blog-layout.tsx
+++ next/ui/blog/blog-layout.tsx
@@ -1,11 +1,11 @@
-import { IconArrowLeft } from "@tabler/icons-react";
-import { Container } from "./container";
-import Image from "next/image";
-import { Link } from "next-view-transitions";
-import { format } from "date-fns";
 import { strapiImage } from "@/lib/strapi/strapiImage";
-import DynamicZoneManager from "./dynamic-zone/manager";
 import { Article } from "@/types/types";
+import { IconArrowLeft } from "@tabler/icons-react";
+import { format } from "date-fns";
+import { Link } from "next-view-transitions";
+import Image from "next/image";
+import { Container } from "../../components/container";
+import DynamicZoneManager from "../../components/dynamic-zone/manager";
 
 export async function BlogLayout({
   article,
@@ -16,68 +16,65 @@ export async function BlogLayout({
   locale: string;
   children: React.ReactNode;
 }) {
-
   return (
     <Container className="mt-16 lg:mt-32">
       <div className="flex justify-between items-center px-2 py-8">
-        <Link href="/blog" className="flex space-x-2 items-center">
+        <Link href="/blog" className="flex items-center space-x-2">
           <IconArrowLeft className="w-4 h-4 text-muted" />
-          <span className="text-sm text-muted">Back</span>
+          <span className="text-muted text-sm">Back</span>
         </Link>
       </div>
-      <div className="w-full mx-auto">
+      <div className="mx-auto w-full">
         {article.image ? (
           <Image
             src={strapiImage(article.image.url)}
             height="800"
             width="800"
-            className="h-40 md:h-96 w-full aspect-square object-cover rounded-3xl [mask-image:radial-gradient(circle,white,transparent)]"
+            className="rounded-3xl w-full h-40 md:h-96 object-cover aspect-square [mask-image:radial-gradient(circle,white,transparent)]"
             alt={article.title}
           />
         ) : (
-          <div className="h-40 md:h-96 w-full aspect-squace rounded-3xl shadow-derek bg-neutral-900 flex items-center justify-center">
+          <div className="flex justify-center items-center bg-neutral-900 shadow-derek rounded-3xl w-full h-40 md:h-96 aspect-squace">
             {/* <Logo /> */}
           </div>
         )}
       </div>
       <div className="xl:relative">
         <div className="mx-auto max-w-2xl">
-          <article className="pb-8 pt-8">
-            <div className="flex gap-4 flex-wrap ">
+          <article className="pt-8 pb-8">
+            <div className="flex flex-wrap gap-4">
               {article.categories?.map((category, idx) => (
                 <p
                   key={`category-${idx}`}
-                  className="text-xs font-bold text-muted px-2 py-1 rounded-full bg-neutral-800 capitalize"
+                  className="bg-neutral-800 px-2 py-1 rounded-full font-bold text-muted text-xs capitalize"
                 >
                   {category.name}
                 </p>
               ))}
             </div>
             <header className="flex flex-col">
-              <h1 className="mt-8 text-4xl font-bold tracking-tight text-neutral-200 sm:text-5xl ">
+              <h1 className="mt-8 font-bold text-neutral-200 text-4xl sm:text-5xl tracking-tight">
                 {article.title}
               </h1>
             </header>
-            <div className="mt-8 prose prose-sm prose-invert">
-              {children}
-            </div>
-            <div className="flex space-x-2 items-center pt-12 border-t border-neutral-800 mt-12">
-              <div className="flex space-x-2 items-center ">
+            <div className="prose-invert mt-8 prose prose-sm">{children}</div>
+            <div className="flex items-center space-x-2 mt-12 pt-12 border-neutral-800 border-t">
+              <div className="flex items-center space-x-2">
                 {/* <Image
                   src={article.authorAvatar}
                   alt={article.author}
                   width={20}
                   height={20}
-                  className="rounded-full h-5 w-5"
+                  className="rounded-full w-5 h-5"
                 />
-                <p className="text-sm font-normal text-muted">
+                <p className="font-normal text-muted text-sm">
                   {article.author}
                 </p> */}
               </div>
-              <div className="h-5 rounded-lg w-0.5 bg-neutral-700" />
+              <div className="bg-neutral-700 rounded-lg w-0.5 h-5" />
               <time
                 dateTime={article.publishedAt}
-                className="flex items-center text-base "
+                className="flex items-center text-base"
               >
                 <span className="text-muted text-sm">
                   {format(new Date(article.publishedAt), "MMMM dd, yyyy")}
@@ -87,7 +84,12 @@ export async function BlogLayout({
           </article>
         </div>
       </div>
-      {article?.dynamic_zone && (<DynamicZoneManager dynamicZone={article?.dynamic_zone} locale={locale} />)}
+      {article?.dynamic_zone && (
+        <DynamicZoneManager
+          dynamicZone={article?.dynamic_zone}
+          locale={locale}
+        />
+      )}
     </Container>
   );
 }
