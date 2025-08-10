d9db15810f9b7d763e4e4c863559140302035382
Alejandro Luis Perez Boza <alpb17.08@gmail.com>
2025-08-10 00:05:30 -0400

    fix: delete blog

--- next/app/[locale]/(marketing)/services/page.tsx
+++ next/app/[locale]/(marketing)/services/page.tsx
@@ -2,14 +2,9 @@ import { Metadata } from "next";
 
 import { AmbientColor } from "@/components/decorations/ambient-color";
 import { Container } from "@/components/container";
-import { Heading } from "@/components/elements/heading";
 import { ProductItems } from "@/components/products/product-items";
-import { Subheading } from "@/components/elements/subheading";
 import fetchContentType from "@/lib/strapi/fetchContentType";
 import { generateMetadataObject } from "@/lib/shared/metadata";
-import { useLocalizedSlugs } from "@/hooks/useLocalizedSlugs";
-
-import ClientSlugHandler from "../ClientSlugHandler";
 
 export async function generateMetadata({
   params
@@ -34,27 +29,14 @@ export default async function Products({
 }: {
   params: { locale: string };
 }) {
-  // Fetch the product-page and products data
-  const servicePage = await fetchContentType("product-pages", {}, true);
   const services = await fetchContentType("products");
 
-  const localizedSlugs = useLocalizedSlugs(
-    servicePage?.localizations,
-    params.locale,
-    "products"
-  );
+  console.log(services, "services");
 
   return (
     <div className="relative overflow-hidden w-full">
-      <ClientSlugHandler localizedSlugs={localizedSlugs} />
       <AmbientColor />
       <Container className="pt-10 pb-40">
-        <Heading as="h1" className="pt-4">
-          {servicePage?.heading}
-        </Heading>
-        <Subheading className="max-w-3xl mx-auto">
-          {servicePage?.sub_heading}
-        </Subheading>
         <ProductItems services={services?.data} locale={params.locale} />
       </Container>
     </div>
